d08f9846ae76c58d6c09002ddaba83bbbafb7520
Lance Toledo <50975456+lancetoledo@users.noreply.github.com>
2022-12-14 18:55:15 -0500

    Intial files

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import '../styles/globals.css'
+// import { MoralisProvider } from 'react-moralis'
+import { PredictionProvider } from '../context/PredictionContext'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <PredictionProvider>
+      <Component {...pageProps} />
+    </PredictionProvider>
+  )
+}
+
+export default MyApp
